5d478f21186c1636444c487abb1f16911b6429f8
Bahriddin <bahridnurullav@gmail.com>
2025-12-09 07:59:09 +0500

    dashboard UI

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,6 +1,6 @@
 import HeadBreadCrumb from "@components/HeadBreadCrumb";
 import React from "react";
-import {Box} from "@chakra-ui/react";
+import {Box, Text} from "@chakra-ui/react";
 import {TripsNeedingAttention} from "./components/TripsNeedingAttention";
 import {PerformanceGrade} from "./components/PerformanceGrade";
 import {SafetyStatus} from "./components/SafetyStatus";
@@ -10,6 +10,14 @@ const Dashboard = () => {
   return (
     <>
       <HeadBreadCrumb />
+      <Text
+        h={"32px"}
+        my="12px"
+        color={"#181D27"}
+        fontWeight={"600"}
+        fontSize={"24px"}>
+        Dashboard
+      </Text>
       <Box py="20px" pb="32px" bg="#f5f5f5" minH="calc(100vh - 80px)">
         <TripsNeedingAttention />
         <PerformanceGrade />
